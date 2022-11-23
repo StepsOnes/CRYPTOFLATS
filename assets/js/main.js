f704bc62b77ec597d97b1e3bac06210596d05a59
My Name <stepan663663@gmail.com>
2022-11-23 22:53:46 +0300

    update-1440 1.5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
 AOS.init();
+
+const boxes2 = Array.from(document.querySelectorAll(".faq__accordion__header"));
+
+boxes2.forEach((box) => {
+    box.addEventListener('click', boxHandler2)
+});
+
+function boxHandler2(e){
+    e.preventDefault();
+    let currentBox = e.target.closest('.faq__accordion__header');
+    let currentBody = currentBox.nextElementSibling;
+    currentBox.classList.toggle('folding-plus')
+
+    currentBody.classList.toggle('active-body');
+
+    if (currentBody.classList.contains('active-body')) {
+        currentBody.style.maxHeight = (currentBody.scrollHeight) + "px"; // открываем контент
+    } else {
+        currentBody.style.maxHeight = 0;
+    }
+}
