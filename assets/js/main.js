b15a4055c8202eb66c7bda68d79d8e204fc65091
My Name <stepan663663@gmail.com>
2022-11-24 21:47:40 +0300

    update-1440 1.5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,28 @@
 AOS.init();
 const circleAnimatePurple = document.querySelector('.circle-animate-purple');
+const circleAnimateGreen = document.querySelector('.circle-animate-green');
+const storyAnimate = document.querySelector('#story-animate');
+const animateImg = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png'];
+
+let i = 0;
+
+setInterval(toggle, 400);
+function toggle() {
+    storyAnimate.src = `assets/img/animation/${animateImg[i++]}`;
+    if (i >8) {
+        i = 0;
+        storyAnimate.src = `assets/img/animation/${animateImg[0]}`;
+    }
+}
 
 document.addEventListener('scroll', function () {
-    let pos = getCoords(circleAnimatePurple).top;
     if (window.scrollY > 2000) {
         circleAnimatePurple.classList.add('circle-animate');
     }
-    console.log(window.scrollY);
-    console.log(pos);
-})
-
-function getCoords(elem) {
-    let box = elem.getBoundingClientRect();
-
-    return {
-        top: box.top + window.pageYOffset,
-        right: box.right + window.pageXOffset,
-        bottom: box.bottom + window.pageYOffset,
-        left: box.left + window.pageXOffset
-    };
-}
+    if (window.scrollY > 2600) {
+        circleAnimateGreen.classList.add('circle-animate');
+    }
+});
 
 const boxes2 = Array.from(document.querySelectorAll(".faq__accordion__header"));
 
@@ -41,3 +44,5 @@ function boxHandler2(e){
         currentBody.style.maxHeight = 0;
     }
 }
+
+
