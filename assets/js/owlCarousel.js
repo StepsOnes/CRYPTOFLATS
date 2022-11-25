6aa088d978cdf47f0816f6dc18cf61141ebf20f6
My Name <stepan663663@gmail.com>
2022-11-25 23:42:24 +0300

    update-1440 1.5

--- assets/js/owlCarousel.js
+++ assets/js/owlCarousel.js
@@ -1,6 +1,19 @@
 
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
+    $('.owl-carousel-1').owlCarousel({
+        items: 8,
+        rtl: true,
+        dots: false,
+        loop: true,
+        autoplay: true,
+        slideTransition: 'linear',
+        autoplayTimeout: 1000,
+        autoplaySpeed: 1000,
+        autoplayHoverPause: false,
+        autoWidth: true
+    });
+
+    $('.owl-carousel-2').owlCarousel({
         items: 8,
         dots: false,
         loop: true,
