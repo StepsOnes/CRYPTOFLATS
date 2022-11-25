73e1416b779b132f5cee89a7d4b3077290abe812
My Name <stepan663663@gmail.com>
2022-11-25 22:59:05 +0300

    update-1440 1.5

--- assets/js/owlCarousel.js
+++ assets/js/owlCarousel.js
@@ -1,6 +1,3 @@
-// function $(document) {
-//
-// }
 
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
@@ -15,18 +12,3 @@ $(document).ready(function(){
         autoWidth: true
     });
 });
-
-// jQuery(function($){ // use jQuery code inside this to avoid "$ is not defined" error
-//     var owl = $('.owl-carousel');
-//     owl.owlCarousel({
-//         items: 8,
-//         dots: false,
-//         loop: true,
-//         autoplay: true,
-//         slideTransition: 'linear',
-//         autoplayTimeout: 1000,
-//         autoplaySpeed: 1000,
-//         autoplayHoverPause: false,
-//         autoWidth: true
-//     });
-// });
