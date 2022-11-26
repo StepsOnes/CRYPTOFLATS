f6101a1397c7006bcee7d35d8c439557523f3691
My Name <stepan663663@gmail.com>
2022-11-26 22:02:23 +0300

    update-1440 1.5

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,3 +63,17 @@ function toggle() {
     }
 }
 
+const gameAnimation = document.querySelector('.game-animation_img');
+const gameAnimationImg = ['THE GAME 1.png', 'THE GAME 2.png', 'THE GAME 3.png', 'THE GAME 4.png', 'THE GAME 5.png'];
+
+setInterval(toggle2, 400);
+
+let j = 0;
+function toggle2() {
+    gameAnimation.src = `assets/img/game_animation/${gameAnimationImg[j++]}`;
+    if (j > 5) {
+        j = 0;
+        gameAnimation.src = `assets/img/game_animation/${gameAnimationImg[0]}`;
+    }
+}
+
