422852408c5064997e0bf5cdef697c73e69c40ad
My Name <stepan663663@gmail.com>
2022-12-03 13:48:41 +0300

    commit

--- assets/js/slideShow.js
+++ assets/js/slideShow.js
@@ -1,18 +1,3 @@
-const storyAnimate = document.querySelector('#story-animate');
-const animateImg = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png'];
-
-let i = 0;
-
-setInterval(toggle, 400);
-
-function toggle() {
-    storyAnimate.src = `assets/img/animation/${animateImg[i++]}`;
-    if (i >8) {
-        i = 0;
-        storyAnimate.src = `assets/img/animation/${animateImg[0]}`;
-    }
-}
-
 const gameAnimation = document.querySelector('.game-animation_img');
 const gameAnimationImg = ['THE GAME 1.png', 'THE GAME 2.png', 'THE GAME 3.png', 'THE GAME 4.png', 'THE GAME 5.png'];
 
