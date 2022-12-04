740aaa778434d4c0ce526ad62ba2bc45e14e5a93
My Name <stepan663663@gmail.com>
2022-12-04 21:30:32 +0300

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,35 +8,46 @@ const openMenu = document.getElementById('open');
 
 const navLinks = document.querySelectorAll('.mobile-nav__item');
 
-const openModalWindow = Array.from(document.querySelectorAll('.about-product__buy-block__btn'));
+const joinWhitelistBtn = Array.from(document.querySelectorAll('.about-product__buy-block__btn'));
 const modalWindow = document.getElementById('modal-window');
 const closeModalWindow = document.querySelector('.close-modal');
 
-modalWindow.addEventListener('click', function (event) {
-    let currentElem = event.target;
-    if (currentElem === closeModalWindow ||currentElem  === modalWindow) {
-        modalWindow.classList.remove('visible');
-    }
-});
+joinWhitelistBtn.forEach((item) => {
+    item.addEventListener('mouseover', function () {
+        this.textContent = "Coming soon";
+    });
 
-document.addEventListener('keydown', function (event) {
-    if (event.keyCode === 27) {
-        modalWindow.classList.remove('visible');
-    }
-});
-
-openModalWindow.forEach((item) => {
-    item.addEventListener('click', function () {
-        modalWindow.classList.add('visible');
+    item.addEventListener('mouseout', function () {
+        this.textContent = "Join whitelist";
     });
 });
 
-navLinks.forEach((item) => {
-    item.addEventListener('click', function () {
-        menu.classList.toggle('not-visible');
-    });
-})
-;
+// modalWindow.addEventListener('click', function (event) {
+//     let currentElem = event.target;
+//     if (currentElem === closeModalWindow ||currentElem  === modalWindow) {
+//         modalWindow.classList.remove('visible');
+//     }
+// });
+
+// document.addEventListener('keydown', function (event) {
+//     if (event.keyCode === 27) {
+//         modalWindow.classList.remove('visible');
+//     }
+// });
+//
+// openModalWindow.forEach((item) => {
+//     item.addEventListener('click', function () {
+//         modalWindow.classList.add('visible');
+//     });
+// });
+//
+// navLinks.forEach((item) => {
+//     item.addEventListener('click', function () {
+//         menu.classList.toggle('not-visible');
+//     });
+// });
+
+
 openMenu.addEventListener('click', function () {
     menu.classList.toggle('not-visible');
 });
