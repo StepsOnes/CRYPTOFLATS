0b095269ef86ec8bc78fd00dc35c78b220af6e06
My Name <stepan663663@gmail.com>
2022-12-20 22:29:50 +0300

    commit

--- assets/js/wowInit.js
+++ assets/js/wowInit.js
@@ -1,1 +1,10 @@
+// circleAnimate = new WOW({
+//     boxClass: 'anim',
+//     animateClass: 'circle-animate',
+//     offset: 0,
+//     mobile: true,
+//     live: true
+// });
+
+
 new WOW().init();
